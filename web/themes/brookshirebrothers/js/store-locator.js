2f22af6e9849beea4cfbd7bc047285565e86b3b1
Charlie Llewellin <charlie@llewellin.net>
2020-05-31 11:13:03 -0500

    store locator code

--- web/themes/brookshirebrothers/js/store-locator.js
+++ web/themes/brookshirebrothers/js/store-locator.js
@@ -1,14 +1,44 @@
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal) {
   "use strict";
   Drupal.behaviors.StoreLocator = {
     attach: function (context, settings) {
       let userZip = $.cookie('userZip');
+      let searchBox = $("#search");
       if (userZip) {
-        $("#search").once("StoreLocator").val(userZip);
+        searchBox.once("StoreLocator").val(userZip);
       }
       $("#moreOptions").once("StoreLocator").click(function() {
         $("#moreOptionsSection").toggle();
       });
+
+      userZip = searchBox.val(); // user might have changed value
+      let storeLocations;
+      let distanceSelect = $('#filter-distance');
+      let overClass = 'over';
+
+      function sendForm() {
+        $.ajax({
+          type: 'get',
+          cache: false,
+          url: form.attr('action'),
+          data: 'ajax=1',
+          dataType: 'text',
+          success: function (data) {
+            let selectedOption = distanceSelect.children().eq(distanceSelect.get(0).selectedIndex);
+            let selectedDistance = parseInt(selectedOption.text(), 10);
+            if (selectedOption.hasClass(overClass)) {
+              selectedDistance = 99999;
+            }
+
+            $(data).appendTo($('#storeResults'));
+
+          }
+        });
+      }
+      $("#searchSubmit").click(function(e){
+        e.preventDefault();
+        sendForm();
+      });
     }
   }
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal);
